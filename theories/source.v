138a385170174b184a049769e567fe20567d9821
Zack Eisbach <zack.eisbach@gmail.com>
2024-05-16 16:25:54 -0400

    tidy up

--- theories/source.v
+++ theories/source.v
@@ -30,35 +30,35 @@ Reserved Notation "Γ ⊢ a : τ '~~>' e" (at level 74, a, τ, e at next level).
 (* NOTE: I fear that overriding with ∪ for merging contexts is actually bad *)
 
 Inductive compile : ctx → aexpr → ty → expr → Prop :=
-  | CUnit Γ : 
+  | CUnit Γ :
       Γ ⊢ Unit : One ~~> "()"
-  | CVar Γ x τ : 
-    Γ !! x = Some τ → 
+  | CVar Γ x τ :
+    Γ !! x = Some τ →
       Γ ⊢ (Var x) : τ ~~> x
-  | CLam Γ x a τ1 τ2 e : 
-    (<[ x := τ1 ]> Γ) ⊢ a : τ1 ~~> e → 
+  | CLam Γ x a τ1 τ2 e :
+    (<[ x := τ1 ]> Γ) ⊢ a : τ1 ~~> e →
       Γ ⊢ (Lam (BNamed x) a) : (Fun τ1 τ2) ~~> λ: x, e
-  | CApp Γ1 Γ2 a1 a2 τ1 τ2 e1 e2 : 
-    Γ1 ⊢ a1 : (Fun τ1 τ2) ~~> e1 → 
-    Γ2 ⊢ a2 : τ1 ~~> e2 → 
+  | CApp Γ1 Γ2 a1 a2 τ1 τ2 e1 e2 :
+    Γ1 ⊢ a1 : (Fun τ1 τ2) ~~> e1 →
+    Γ2 ⊢ a2 : τ1 ~~> e2 →
       (Γ1 ∪ Γ2) ⊢ (App a1 a2) : τ2 ~~> e1 e2
-  | CPair Γ1 Γ2 a1 a2 τ1 τ2 e1 e2 : 
-    Γ1 ⊢ a1 : τ1 ~~> e1 → 
-    Γ2 ⊢ a2 : τ2 ~~> e2 → 
+  | CPair Γ1 Γ2 a1 a2 τ1 τ2 e1 e2 :
+    Γ1 ⊢ a1 : τ1 ~~> e1 →
+    Γ2 ⊢ a2 : τ2 ~~> e2 →
       (Γ1 ∪ Γ2) ⊢ (Pair a1 a2) : (Tensor τ1 τ2) ~~> (e1, e2)
-  | CSplit Γ1 Γ2 x1 x2 a1 a2 τ1 τ2 τ e1 e2 : 
-    Γ1 ⊢ a1 : (Tensor τ1 τ2) ~~> e1 → 
-    (<[ x2 := τ2 ]> (<[ x1 := τ1 ]> Γ2)) ⊢ a2 : τ ~~> e2 → 
-      (Γ1 ∪ Γ2) ⊢ (Split (BNamed x1) (BNamed x2) a1 a2) : τ ~~> 
-        let: "tmp" := e1 in 
-        let: x1 := Fst "tmp" in 
+  | CSplit Γ1 Γ2 x1 x2 a1 a2 τ1 τ2 τ e1 e2 :
+    Γ1 ⊢ a1 : (Tensor τ1 τ2) ~~> e1 →
+    (<[ x2 := τ2 ]> (<[ x1 := τ1 ]> Γ2)) ⊢ a2 : τ ~~> e2 →
+      (Γ1 ∪ Γ2) ⊢ (Split (BNamed x1) (BNamed x2) a1 a2) : τ ~~>
+        let: "tmp" := e1 in
+        let: x1 := Fst "tmp" in
         let: x2 := (Snd "tmp") in e2
   | CNew Γ a τ e :
-    Γ ⊢ a : τ ~~> e → 
+    Γ ⊢ a : τ ~~> e →
       Γ ⊢ (New a) : (Unq τ) ~~> ref e
   | CSwap Γ1 Γ2 a1 a2 τ1 τ2 e1 e2 :
-    Γ1 ⊢ a1 : (Unq τ1) ~~> e1 → 
-    Γ2 ⊢ a2 : τ2 ~~> e2 → 
+    Γ1 ⊢ a1 : (Unq τ1) ~~> e1 →
+    Γ2 ⊢ a2 : τ2 ~~> e2 →
       (Γ1 ∪ Γ2) ⊢ (Swap a1 a2) : (Tensor (Unq τ2) τ1) ~~>
         let: "l" := e1 in
         let: "r" := !"l" in
