42e223152c8c587da846151b80d2ee8611416077
Zack Eisbach <zack.eisbach@gmail.com>
2024-05-27 15:36:19 -0400

    a definition that sorta goes through

--- theories/ref_count/dagree.v
+++ theories/ref_count/dagree.v
@@ -1,30 +1,25 @@
+(* https://gitlab.mpi-sws.org/iris/iris/blob/master/iris/algebra/agree.v *)
 From iris.algebra Require Export cmra.
 From iris.prelude Require Import options.
 
-(* discrete CMRA (aka RA) for agreement. using the built-in CMRA
-   runs into positivity issues, since the carrier is non-empty lists *)
+(* ZACK NOTE: more `Local Arguments ...` would go here *)
 
-(* structure inspired by
-  https://gitlab.mpi-sws.org/iris/iris/blob/master/iris/algebra/excl.v 
-*)
+Record dagree (A : Type) := Agree {
+  dagree_head : A;
+  dagree_tail : list A
+}.
+Global Arguments Agree {_}.
+Global Arguments dagree_head {_}.
+Global Arguments dagree_tail {_}.
 
-(* ZACK NOTE: `Local Arguments ...` go here *)
+Definition to_agree {A} (a : A) : dagree A :=
+  {| dagree_head := a; dagree_tail := [] |}.
 
-Inductive dagree (A : Type) :=
-  | Agree : A → dagree A
-  | Disagree : dagree A.
-(* A is implicit and can be inferred *)
-Global Arguments Agree {_} _.
-Global Arguments Disagree {_}.
+Definition to_list {A} (xxs : dagree A) : list A :=
+  dagree_head xxs :: dagree_tail xxs.
+Local Coercion to_list : dagree >-> list.
 
-(* specifies expected parameters through the Params typeclass,
-   apparently @ allows explicit type arguments, implicits *)
-Global Instance: Params (@Agree) 1 := {}.
-Global Instance: Params (@Disagree) 1 := {}.
-
-Global Instance maybe_Excl {A} : Maybe (@Agree A) := λ x,
-  match x with Agree a => Some a | _ => None end.
-
+(* More equality stuff would go here, as needed *)
 
 Section dagree.
   Context {A : Type}.
@@ -34,26 +29,27 @@ Section dagree.
   (* Lifts (=) to Equiv, making (dagree A) an OFE *)
   Canonical Structure dagreeO := leibnizO (dagree A).
 
+  Local Instance agree_op_instance : Op (dagree A) :=
+    λ x y, Agree (dagree_head x) (dagree_tail x ++ y).
+
   Local Instance dagree_valid_instance : Valid (dagree A) := λ x,
-    match x with Agree _ => True | Disagree => False end.
+    (* Alternative would be to lift more things like ∈ to dagree,
+       or to inline. this can be changed to be more ergonomic *)
+    let x := to_list x in 
+      match x with
+      | [a] => True
+      | _ => ∀ a b, a ∈ x → b ∈ x → a = b
+      end.
 
-  (* This directly emulates Iris's PCore instance for `agree`,
-     even though I'm not entirely convinced this is what we want *)
-  Local Instance dagree_pcore_instance : PCore (dagree A) := Some.
-
-  Local Instance dagree_op_instance : Op (dagree A) := λ x y,
-    match x, y with 
-    | Agree x, Agree y => if (decide (x = y)) then Agree x else Disagree
-    | _, _ => Disagree
-    end.
+  Local Instance agree_pcore_instance : PCore (dagree A) := Some.
 
   Definition dagree_mixin : RAMixin (dagree A).
   Proof.
+    (* Insert proof here (annoying, but should work with helpers) *)
     Admitted.
 
   Canonical Structure dagreeR := discreteR (dagree A) dagree_mixin.
 
-  (* https://gitlab.mpi-sws.org/iris/iris/blob/master/iris/algebra/agree.v *)
   (* There may be more things in here that are needed, like certain
      associativity, injectivity, etc things, more helper lemmas ... *)
   Global Instance dagree_cmra_total : CmraTotal dagreeR.
@@ -66,4 +62,4 @@ Section dagree.
   Proof. apply discrete_cmra_discrete. Qed.
 End dagree.
 
-Global Arguments dagreeR _ {_}.
+Global Arguments dagreeR : clear implicits.
--- theories/ref_count/foo.v
+++ theories/ref_count/foo.v
@@ -5,38 +5,50 @@ From iris.heap_lang Require Import proofmode.
 From iris.heap_lang Require Import notation lang.
 From iris.algebra Require Import numbers csum excl gmap.
 From iris.prelude Require Import options.
-From affi.ref_count Require Import dagree_eqd.
+From affi.ref_count Require Import dagree.
 
-(* FAIL: cellR needs resR to be EqDecision to define,
-   but it can't be EqDecision until it is actually defined... *)
+(* IDEA: define a new version of `agree` that uses `option`
+   FAILS: requires {EqDecision A} to define composition, preventing
+   us from using it inductively: 
 
 Definition cellR (A : Type) `{EqDecision A} : cmra := 
   csumR (exclR (leibnizO val)) (prodR positiveR (dagreeR A)).
 
-(* FAILS:
-  Inductive resR := 
-  | Res : gmapR loc (cellR resR) -> resR.
-*)
+Inductive resR := 
+| Res : gmapR loc (cellR resR) -> resR. *)
 
 (* Therefore, we need to try something that doesn't use EqDecision *)
 
-(* IDEA: try to use the gset version from Semantics notes
-   - FAILS: they also need EqDecision for non-empty check
-   IDEA: try to use standard list version
+(* IDEA: try to use standard list version
    - FAILS: due to positivity 
+
+   IDEA: try to use the gset version from Semantics notes
+   - FAILS: they also need EqDecision for non-empty check
+
    IDEA: use standard list version, but _without_ non-empty restriction? 
    - FAILS: literally not a resource algebra: ✓ (emp ⋅ good) but NOT ✓ emp 
-   IDEA: use a jank list version for non-empty... 
 *)
 
-(* FAILS:
-  Inductive baz := 
-  | Baz : gmap nat (agree (leibnizO baz)) -> baz.
-*)
+(* CURRENT VERSION: use a new version of agree that still has non-empty
+   list underlying, but uses a different, strictly positive definition *)
 
+Definition cellR (A : Type) : cmra := 
+  csumR (exclR (leibnizO val)) (prodR positiveR (dagreeR A)).
+
+Inductive resR := 
+  | Res : gmapR loc (cellR resR) -> resR.
+
+(* Now, the notion of validity here is lacking, so we need to define
+   our own. Luckily, we can easily augment resR with a new notion
+   using iso_cmra_mixin_restrict_validity *)
+
+(* ANOTHER OPTION (probably easiest...): just define everything
+   directly over the inductive carrier, optionally picking 
+   definitions up from pre-existing combinators 
 
 Inductive res :=
 | Res : gmap loc cell -> res
 with cell :=
 | Own : val -> cell
-| Shr : positive -> res -> cell.
+| Shr : positive -> res -> cell. 
+*)
